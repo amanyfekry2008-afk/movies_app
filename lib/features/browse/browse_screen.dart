f059b36713bfabc638ea3ecacba7f9c5e457a977
Amany Fekry <amanyfekry2008@gmail.com>
2026-06-03 18:52:48 +0300

    add search and browse ui

--- lib/features/browse/browse_screen.dart
+++ lib/features/browse/browse_screen.dart
@@ -1,12 +1,91 @@
 import 'package:flutter/material.dart';
+import 'package:movies_app/core/utils/app_colors.dart';
+import 'package:movies_app/core/utils/app_images.dart';
+import 'package:movies_app/core/widgets/movie_card/movie_card.dart';
 
 class BrowseScreen extends StatelessWidget {
   const BrowseScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final genres = [
+      'Action',
+      'Adventure',
+      'Animation',
+      'Drama',
+      'Comedy',
+    ];
+
+    final movies = [
+      AppImages.movie1,
+      AppImages.movie2,
+      AppImages.movie3,
+      AppImages.movie1,
+      AppImages.movie2,
+      AppImages.movie3,
+    ];
+
     return Scaffold(
-      body: Text("Browse "),
+      backgroundColor: AppColors.black,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: 40,
+                child: ListView.separated(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: genres.length,
+                  separatorBuilder: (context, index) =>
+                  const SizedBox(width: 8),
+                  itemBuilder: (context, index) {
+                    return Container(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 16,
+                      ),
+                      decoration: BoxDecoration(
+                        color: AppColors.yellow,
+                        borderRadius:
+                        BorderRadius.circular(16),
+                      ),
+                      alignment: Alignment.center,
+                      child: Text(
+                        genres[index],
+                        style: const TextStyle(
+                          color: AppColors.black,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+
+              const SizedBox(height: 20),
+
+              Expanded(
+                child: GridView.builder(
+                  itemCount: movies.length,
+                  gridDelegate:
+                  const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    crossAxisSpacing: 12,
+                    mainAxisSpacing: 12,
+                    childAspectRatio: 0.67,
+                  ),
+                  itemBuilder: (context, index) {
+                    return MovieCard(
+                      image: movies[index],
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/features/search/search_screen.dart
+++ lib/features/search/search_screen.dart
@@ -1,12 +1,85 @@
 import 'package:flutter/material.dart';
+import 'package:movies_app/core/utils/app_colors.dart';
+import 'package:movies_app/core/utils/app_images.dart';
+import 'package:movies_app/core/widgets/movie_card/movie_card.dart';
 
 class SearchScreen extends StatelessWidget {
   const SearchScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final movies = [
+      AppImages.movie1,
+      AppImages.movie2,
+      AppImages.movie3,
+      AppImages.movie1,
+      AppImages.movie2,
+      AppImages.movie3,
+    ];
+
     return Scaffold(
-      body: Text("search"),
+      backgroundColor: AppColors.black,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            children: [
+              TextFormField(
+                style: const TextStyle(
+                  color: AppColors.white,
+                ),
+                decoration: InputDecoration(
+                  hintText: 'Search',
+                  hintStyle: const TextStyle(
+                    color: AppColors.white,
+                  ),
+                  prefixIcon: const Icon(
+                    Icons.search,
+                    color: AppColors.white,
+                  ),
+                  filled: true,
+                  fillColor: AppColors.grey,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(16),
+                    borderSide: BorderSide.none,
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(16),
+                    borderSide: BorderSide.none,
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(16),
+                    borderSide: const BorderSide(
+                      color: AppColors.yellow,
+                      width: 2,
+                    ),
+                  ),
+                ),
+              ),
+
+              const SizedBox(height: 20),
+
+              Expanded(
+                child: GridView.builder(
+                  itemCount: movies.length,
+                  gridDelegate:
+                  const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    crossAxisSpacing: 12,
+                    mainAxisSpacing: 12,
+                    childAspectRatio: 0.67,
+                  ),
+                  itemBuilder: (context, index) {
+                    return MovieCard(
+                      image: movies[index],
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
