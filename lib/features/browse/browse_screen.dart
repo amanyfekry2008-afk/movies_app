644925429c2dc5989a5e3139451686c528872481
Amany Fekry <amanyfekry2008@gmail.com>
2026-06-03 19:00:05 +0300

    add search and browse ui

--- lib/features/browse/browse_screen.dart
+++ lib/features/browse/browse_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:movies_app/core/utils/app_colors.dart';
 import 'package:movies_app/core/utils/app_images.dart';
+import 'package:movies_app/core/utils/app_text.dart';
 import 'package:movies_app/core/widgets/movie_card/movie_card.dart';
 
 class BrowseScreen extends StatelessWidget {
@@ -29,9 +30,13 @@ class BrowseScreen extends StatelessWidget {
       backgroundColor: AppColors.black,
       body: SafeArea(
         child: Padding(
-          padding: const EdgeInsets.all(16),
+          padding: const EdgeInsets.symmetric(
+            horizontal: 16,
+            vertical: 12,
+          ),
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+            crossAxisAlignment:
+            CrossAxisAlignment.start,
             children: [
               SizedBox(
                 height: 40,
@@ -42,20 +47,28 @@ class BrowseScreen extends StatelessWidget {
                   const SizedBox(width: 8),
                   itemBuilder: (context, index) {
                     return Container(
-                      padding: const EdgeInsets.symmetric(
+                      padding:
+                      const EdgeInsets.symmetric(
                         horizontal: 16,
                       ),
                       decoration: BoxDecoration(
-                        color: AppColors.yellow,
+                        color: index == 0
+                            ? AppColors.yellow
+                            : AppColors.grey,
                         borderRadius:
                         BorderRadius.circular(16),
                       ),
                       alignment: Alignment.center,
                       child: Text(
                         genres[index],
-                        style: const TextStyle(
-                          color: AppColors.black,
-                          fontWeight: FontWeight.w500,
+                        style:
+                        AppText.regular.copyWith(
+                          color: index == 0
+                              ? AppColors.black
+                              : AppColors.white,
+                          fontSize: 14,
+                          fontWeight:
+                          FontWeight.w500,
                         ),
                       ),
                     );
@@ -71,8 +84,8 @@ class BrowseScreen extends StatelessWidget {
                   gridDelegate:
                   const SliverGridDelegateWithFixedCrossAxisCount(
                     crossAxisCount: 2,
-                    crossAxisSpacing: 12,
-                    mainAxisSpacing: 12,
+                    crossAxisSpacing: 14,
+                    mainAxisSpacing: 14,
                     childAspectRatio: 0.67,
                   ),
                   itemBuilder: (context, index) {
--- lib/features/search/search_screen.dart
+++ lib/features/search/search_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:movies_app/core/utils/app_colors.dart';
 import 'package:movies_app/core/utils/app_images.dart';
+import 'package:movies_app/core/utils/app_text.dart';
 import 'package:movies_app/core/widgets/movie_card/movie_card.dart';
 
 class SearchScreen extends StatelessWidget {
@@ -21,37 +22,31 @@ class SearchScreen extends StatelessWidget {
       backgroundColor: AppColors.black,
       body: SafeArea(
         child: Padding(
-          padding: const EdgeInsets.all(16),
+          padding: const EdgeInsets.symmetric(
+            horizontal: 16,
+            vertical: 12,
+          ),
           child: Column(
             children: [
-              TextFormField(
-                style: const TextStyle(
-                  color: AppColors.white,
+              Container(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 12,
                 ),
-                decoration: InputDecoration(
-                  hintText: 'Search',
-                  hintStyle: const TextStyle(
-                    color: AppColors.white,
-                  ),
-                  prefixIcon: const Icon(
-                    Icons.search,
-                    color: AppColors.white,
-                  ),
-                  filled: true,
-                  fillColor: AppColors.grey,
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(16),
-                    borderSide: BorderSide.none,
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(16),
-                    borderSide: BorderSide.none,
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(16),
-                    borderSide: const BorderSide(
-                      color: AppColors.yellow,
-                      width: 2,
+                decoration: BoxDecoration(
+                  color: AppColors.grey,
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                child: TextFormField(
+                  style: AppText.regular,
+                  decoration: const InputDecoration(
+                    border: InputBorder.none,
+                    hintText: 'Search',
+                    hintStyle: TextStyle(
+                      color: AppColors.white,
+                    ),
+                    prefixIcon: Icon(
+                      Icons.search,
+                      color: AppColors.white,
                     ),
                   ),
                 ),
@@ -65,8 +60,8 @@ class SearchScreen extends StatelessWidget {
                   gridDelegate:
                   const SliverGridDelegateWithFixedCrossAxisCount(
                     crossAxisCount: 2,
-                    crossAxisSpacing: 12,
-                    mainAxisSpacing: 12,
+                    crossAxisSpacing: 14,
+                    mainAxisSpacing: 14,
                     childAspectRatio: 0.67,
                   ),
                   itemBuilder: (context, index) {
