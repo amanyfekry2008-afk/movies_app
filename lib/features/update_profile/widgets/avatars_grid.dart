299e8099399d03a0e2a65311f0ca85834d30e5b8
Amany Fekry <amanyfekry2008@gmail.com>
2026-06-04 23:42:48 +0300

    home logic update

--- lib/features/update_profile/widgets/avatars_grid.dart
+++ lib/features/update_profile/widgets/avatars_grid.dart
@@ -3,7 +3,6 @@ import 'package:movies_app/core/utils/app_colors.dart';
 import 'package:movies_app/core/utils/app_images.dart';
 
 class AvatarsGrid extends StatelessWidget {
-
   final String selectedAvatar;
 
   final Function(String) onAvatarSelected;
@@ -16,9 +15,7 @@ class AvatarsGrid extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     final avatars = [
-
       AppImages.avatar,
       AppImages.avatar,
       AppImages.avatar,
@@ -32,59 +29,42 @@ class AvatarsGrid extends StatelessWidget {
     ];
 
     return GridView.builder(
-
       shrinkWrap: true,
 
-      physics:
-      const NeverScrollableScrollPhysics(),
+      physics: const NeverScrollableScrollPhysics(),
 
       itemCount: avatars.length,
 
-      gridDelegate:
-      const SliverGridDelegateWithFixedCrossAxisCount(
-
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
         crossAxisCount: 3,
-
         crossAxisSpacing: 12,
         mainAxisSpacing: 12,
       ),
-
       itemBuilder: (context, index) {
-
         final avatar = avatars[index];
-
-        final isSelected =
-            avatar == selectedAvatar;
+        final isSelected = avatar == selectedAvatar;
 
         return GestureDetector(
-
           onTap: () {
             onAvatarSelected(avatar);
           },
 
           child: Container(
-
             padding: const EdgeInsets.all(3),
 
             decoration: BoxDecoration(
-
-              borderRadius:
-              BorderRadius.circular(18),
+              borderRadius: BorderRadius.circular(18),
 
               border: Border.all(
                 color: isSelected
                     ? AppColors.yellow
-                    : AppColors.yellow
-                    .withValues(alpha: 0.5),
+                    : AppColors.yellow.withValues(alpha: 0.5),
 
                 width: 1.5,
               ),
             ),
 
-            child: CircleAvatar(
-              backgroundImage:
-              AssetImage(avatar),
-            ),
+            child: CircleAvatar(backgroundImage: AssetImage(avatar)),
           ),
         );
       },
